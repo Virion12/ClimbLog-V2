7e1f1941eb5d20ae7e315c538d1378161327f02f
Virion12 <dgiwebapps@gmail.com>
2025-09-14 20:30:41 +0200

    Simple visualization of routes

--- climblog_mobile/lib/Widgets/Routes/all_routes.dart
+++ climblog_mobile/lib/Widgets/Routes/all_routes.dart
@@ -1,4 +1,6 @@
 import 'package:climblog_mobile/Riverpod/local_routes_riverpod.dart';
+import 'package:climblog_mobile/Widgets/Routes/route_add_button.dart';
+import 'package:climblog_mobile/Widgets/Routes/route_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,24 +18,37 @@ class _AllRoutesState extends ConsumerState<AllRoutes> {
     return routesAsync.when(
         data: (routes) {
           if (routes.isEmpty) {
-            return const Center(child: Text("Brak dróg w bazie"));
+            return
+            RouteAddButton();
           }
-          return ListView.builder(
-            itemCount: routes.length,
-            itemBuilder: (context, index) {
-              final route = routes[index];
-              return ListTile(
-                title: Text(route.name),
-                subtitle: Text("Color: ${route.color}, Height: ${route.height}"),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () async {
-                    final service = ref.read(routeServiceProvider);
-                    await service.removeRoute(route.id);
-                  },
-                ),
-              );
-            },
+          return Wrap(
+            spacing: 4,
+            runSpacing: 4,
+            children: [GestureDetector(
+                    onTap: () { Navigator.pushNamed(context, '/addRoute2');},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: const Color(0xFF00a896),
+                      ),
+                      padding: EdgeInsets.only(top: 8,bottom: 5),
+                      height: 55,
+                      width: 55,
+                      margin: EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
+                      child: Icon(Icons.add, size: 30, color: Colors.white,),
+                    ),
+                  ),
+                  
+                  ...List.generate(routes.length,(index){
+                    final route = routes[index];
+                    return GestureDetector(
+                       onTap: () { 
+                        //add in future route redirection
+                       },
+                      child:RouteCard(color: route.color, grade: route.grade,timestamp: route.createdAt)
+                    );
+                  })
+                  ]
           );
         },
         loading: () => const Center(child: CircularProgressIndicator()),
---
+++ climblog_mobile/lib/Widgets/Routes/route_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class RouteCard extends StatelessWidget {
+    final String color;
+    final String grade;
+   
+    final DateTime timestamp;
+  const RouteCard({super.key, required this.color, required this.grade,required this.timestamp});
+
+  @override
+  Widget build(BuildContext context) {
+
+   
+
+
+    
+    Color bordercolor = Colors.black;
+
+    return  Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          
+          padding: EdgeInsets.only(top: 8,bottom: 5),
+          height: 55,
+          width: 55,
+          margin: EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(grade,style: TextStyle(fontWeight: FontWeight.bold),),
+              Container(
+                decoration: BoxDecoration(
+                  color: bordercolor,
+                  border: Border.all(
+                    color: bordercolor,
+                    width: 1.0,
+                    
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                
+                width: 35,
+                height: 5,
+              ),
+              Text("${timestamp.day}.${timestamp.month}.${timestamp.year - 2000}",
+              style: TextStyle(
+                fontSize: 10
+              ),),
+              
+            ],
+          )
+        );
+  }
+}
